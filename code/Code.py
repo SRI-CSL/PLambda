056f8779579382674e880154b8d3f98ab96c46a0
Ian A. Mason <iam@csl.sri.com>
2016-05-24 20:16:36 -0700

    Enuf 4 1 day.

---
+++ code/Code.py
@@ -0,0 +1,14 @@
+
+
+class Code(object):
+
+    def __init__(self, spine, filename, lineno):
+        self.spine = tuple(spine)
+        self.filename = filename
+        self.lineno = lineno
+
+
+        
+
+
+    
