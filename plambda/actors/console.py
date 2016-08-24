7cf914581acc18ed171bd20c9bce6752b283e50c
Ian A. Mason <iam@csl.sri.com>
2016-08-24 15:10:58 -0700

    The beginnings of a console for clt.

---
+++ plambda/actors/console.py
@@ -0,0 +1,43 @@
+import Tkinter as tk
+
+
+class TextArea(tk.Frame):
+     def __init__(self):
+         tk.Frame.__init__(self, borderwidth=1, relief="sunken")
+         self.top = tk.Text(wrap="word", background="white", borderwidth=0, highlightthickness=0)
+         self.top_sb = tk.Scrollbar(orient="vertical", borderwidth=1, command=self.top.yview)
+         self.top.configure(yscrollcommand=self.top_sb.set)
+         self.top_sb.pack(in_=self, side="right", fill="y", expand=False)
+         self.top.pack(in_=self, side="left", fill="both", expand=True)
+
+
+class Console(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        
+        self.console_frame = tk.Frame(borderwidth=1, relief="sunken")
+
+        self.toolbar = tk.Frame()
+
+        self.splitpane = tk.PanedWindow(orient=tk.VERTICAL, sashwidth=10, showhandle=True, sashrelief=tk.RAISED)
+        self.splitpane.pack(fill=tk.BOTH, expand=1)
+
+        self.top_frame = TextArea()
+        
+        self.splitpane.add(self.top_frame)
+
+        self.bottom_frame = TextArea()
+        
+        self.splitpane.add(self.bottom_frame)
+
+        self.splitpane.pack(in_=self.console_frame, side="left", fill="both", expand=True)
+
+        self.toolbar.pack(in_=self.console_frame, side="top", fill="x")
+
+        self.console_frame.pack(side="bottom", fill="both", expand=True)
+
+        
+if __name__ == "__main__":
+    console=Console()
+    console.mainloop()
+
