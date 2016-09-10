fd430b7d15d5dadf064ba4ccd4b4b70ffd19e289
Ian A. Mason <iam@csl.sri.com>
2016-09-09 17:11:14 -0700

    Cleanup.

--- tests/Testing.py
+++ tests/Testing.py
@@ -24,19 +24,9 @@ class PLambdaTest(unittest.TestCase):
     def plambdaExceptionTest(self, string, value):
         try:
             retval = self.interpreter.evaluateString(string)
-
-            if str(retval) != str(value):
-                print 'str(retval): ', str(retval)
-                print 'str(value): ', str(value)
-            
             self.assertEqual(str(retval), str(value))
         except Exception as e:
             #traceback.print_exc(file=sys.stderr)
-            print 'type(e): ', type(e)
-            print 'type(value): ', type(value)
-            
             self.assertEqual(type(e), type(value))
-            print 'str(e): ', str(e)
-            print 'str(value): ', str(value)
             self.assertEqual(str(e), str(value))
 
