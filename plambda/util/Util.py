0637b2ecb06ec283f72eab81566c4239da22b765
Ian A. Mason <iam@csl.sri.com>
2016-10-05 09:38:03 -0700

    The usual utilities for easy file io

--- plambda/util/Util.py
+++ plambda/util/Util.py
@@ -10,3 +10,26 @@ def isInteger(data):
     if (sys.version_info[0] == 3):
         return isinstance(data, int)
     return isinstance(data, (int, long))
+
+
+
+def string2File(string, path, append):
+    retval = False
+    try:
+        mode = 'a' if append else 'w'
+        with open(path, mode) as fp:
+            fp.write(string)
+        retval = True
+    except Exception as e:
+        sys.stderr.write('string2File(..., {0}, ...) threw {1}\n'.format(path, str(e)))
+    return retval
+
+
+def file2String(path):
+    retval = None
+    try:
+        with open(path, 'r') as fp:
+            retval = fp.read()
+    except Exception as e:
+        sys.stderr.write('file2String("{0}") threw {1}\n'.format(path, str(e)))
+    return retval
