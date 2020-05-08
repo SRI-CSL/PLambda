43aae88f38520f6e90e0f959f3f35cf4575abe31
Ian A.Mason <iam.csl.sri.com>
2020-05-08 13:08:16 -0700

    version bump.

--- plambda/version.py
+++ plambda/version.py
@@ -8,7 +8,7 @@ Version history:
 
 1.0.0   7/1/2016    Paging in the pip process.
 
-1.0.1   8/5/2016    pyactor shutsdown (i.e. kills) any children (like SITL etc).
+1.0.1   8/5/2016    pyactor shuts down (i.e. kills) any children (like SITL etc).
 
 1.0.2   8/18/2016   kwapply added.
 
@@ -24,7 +24,7 @@ Version history:
 1.0.7   8/27/2016   Updated all relative imports to follow:
                     https://www.python.org/dev/peps/pep-0328/
 
-1.0.8   9/02/2016   CPS (Continuation Passing Stype) installed as the default.
+1.0.8   9/02/2016   CPS (Continuation Passing Style) installed as the default.
                     Can switch to recursive via a mode in plambda.eval.Interpreter
 
 1.0.9   9/10/2016   Various bug fixes in the CPS interpreter.
@@ -37,8 +37,8 @@ Version history:
 
 1.0.13  11/15/2016  pylintification complete.
 
-
+1.1.0   05/08/2020  ditch 2.7, dust off the cobwebs, lint the puppy, and forge on.
 
 """
 
-plambda_version = '1.0.13'
+plambda_version = '1.1.0'
